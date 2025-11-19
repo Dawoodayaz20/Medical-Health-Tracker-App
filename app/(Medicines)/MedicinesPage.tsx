fab49180c70da5fe5ec43719c0de9a71d03bde9b
Dawood Ayaz <dawoodayaz18@gmail.com>
2025-11-19 19:52:46 +0500

    Add medicine time with other medicine details.

--- app/(Medicines)/MedicinesPage.tsx
+++ app/(Medicines)/MedicinesPage.tsx
@@ -28,48 +28,6 @@ export default function MedicinesPage() {
     )
 
   return (
-//     <View className="flex-1 bg-blue-50 p-6 mt-6">
-//       {/* Header */}
-//       <View className="flex-row justify-between items-center mb-6">
-//         <ArrowLeft onPress={(() => router.back())} size={22} color="black" />
-//         <Text className="text-xl font-bold text-blue-700">Medicines</Text>
-//         <TouchableOpacity
-//           onPress={() => router.push("/AddMedicines")}
-//           className="bg-blue-600 p-2 rounded-full"
-//         >
-//           <Plus color="white" size={22} />
-//         </TouchableOpacity>
-//       </View>
-
-//       {/* List */}
-//       <FlatList
-//         data={medicine}
-//         keyExtractor={(item) => item.id.toString()}
-//         renderItem={({ item }) => (
-//         <TouchableOpacity
-//             activeOpacity={0.7}
-//             className="bg-white rounded-xl p-4 mb-4 shadow-sm border border-gray-200"
-//             >
-//             <View className="flex-row justify-between items-start">
-//             <View>
-//             <Text className="text-lg font-semibold text-gray-800">{item.name}</Text>
-//             <Text className="text-sm text-gray-500 mt-1">For: {item.illness}</Text>
-//             <Text className="text-sm text-gray-500 mt-1">
-//                  Dosage: {item.dosage}
-//             </Text>
-//             <Text className="text-sm text-gray-500 mt-1">{item.detail}</Text>
-//             </View>
-//             <TouchableOpacity>
-//             <Trash2 onPress={(() => DeleteMedicine(item.id))} size={20} color="#ef4444" /> 
-//             </TouchableOpacity>
-//             </View>
-//         </TouchableOpacity>
-//         )}
-//       />
-//     </View>
-//     )
-// }
-
     <View style={styles.container}>
       {/* Header */}
       <View style={styles.header}>
@@ -98,6 +56,7 @@ export default function MedicinesPage() {
                 <Text style={styles.medicineName}>{item.name}</Text>
                 <Text style={styles.medicineDetail}>For: {item.illness}</Text>
                 <Text style={styles.medicineDetail}>Dosage: {item.dosage}</Text>
+                <Text style={styles.medicineDetail}>Timing: {item.dosage}</Text>
                 <Text style={styles.medicineDetail}>{item.detail}</Text>
               </View>
               <TouchableOpacity onPress={() => DeleteMedicine(item.id)}>
